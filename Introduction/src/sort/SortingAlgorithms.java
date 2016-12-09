defe3a4e303466ff6f04c89b2e019f827fef2bb9
Student6 <Student6@10.8.33.107>
2016-12-09 12:32:48 -0500

    draw

--- Introduction/src/sort/SortingAlgorithms.java
+++ Introduction/src/sort/SortingAlgorithms.java
@@ -7,9 +7,10 @@ public class SortingAlgorithms {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arrayToSort={133,0,-5,31,12,12,2};
-		System.out.println("- - - SELECTION SORT - - -");
-		System.out.println(Arrays.toString(arrayToSort));
-		selectionSort(copy(arrayToSort));
+//		System.out.println("- - - QUICK SORT - - -");
+//		System.out.println(Arrays.toString(arrayToSort));
+//		quickSort(copy(arrayToSort),0,arrayToSort.length-1);
+		sort(copy(arrayToSort));
 	}
 
 	public static int[] copy(int[] arr){
@@ -80,6 +81,84 @@ public class SortingAlgorithms {
 				}
 			}
 			//keep doing that repeatedly, eventually the largest values will "float" to the top
+		}
+	}
+	public static int[] merge(int[] partA, int[] partB){
+		System.out.println("Merging "+Arrays.toString(partA)+ " and "+Arrays.toString(partB));
+		int aIndex = 0;
+		int bIndex = 0;
+		int[] merge = new int[partA.length + partB.length];
+		//merge index
+		int i = 0;
+		//compare first element in each array, take the smaller of the two
+		while(aIndex < partA.length && bIndex < partB.length){
+			if(partA[aIndex] < partB[bIndex]){
+				merge[i]=partA[aIndex];
+				aIndex++;
+				i++;
+			}else{
+				merge[i]=partB[bIndex];
+				bIndex++;
+				i++;
+			}
+		}
+		//add leftovers
+		while(aIndex < partA.length){
+			merge[i]=partA[aIndex];
+			aIndex++;
+			i++;
+		}
+		while(bIndex < partB.length){
+			merge[i]=partB[bIndex];
+			bIndex++;
+			i++;
+		}
+		System.out.println("...Result: "+Arrays.toString(merge));
+		return merge;
+	}
+	
+	
+	//since quickSort is recursive, the start and end must be specified 
+	//(to break it down through recursive calls)
+	public static void quickSort(int[] arrayToSort, int start, int end){
+		//trivial case is when the array is empty or contains one element
+		//sort only if this is not the case, (otherwise nothing is done)
+		if(start<end){			
+			//choose the pivot from the end
+			int pivot = arrayToSort[end];
+			System.out.println("PIVOT "+pivot);
+			//pIndex is the index where the pivot will go at the end.
+			//It is also the number of elements SMALLER than the pivot
+			//(note: pivot cannot be smaller than start, since this is only
+			//sorting elements after start)
+			int pIndex = start;
+			//compare every element to the pivot
+			for(int i=start; i<end; i++){
+				if(arrayToSort[i]<pivot){
+					swap(arrayToSort,i,pIndex);
+					//pIndex increases because there is another element smaller than the pivot
+					pIndex++;
+				}
+			}
+			//now put the pivot in the correct spot
+			swap(arrayToSort,pIndex, end);
+			//and sort the elements on the left and right of the pivot
+			if(start<pIndex-1)quickSort(arrayToSort, start, pIndex-1);
+			if(pIndex-1 < end)quickSort(arrayToSort, pIndex+1, end);
+		}
+	}
+	public static void sort(int[] array){
+		for(int i=0;i<array.length; i++){
+			for(int j = 0; j<array.length -1- i; j++){
+				if(array[j]>array[j+1]){
+					int temp = array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+				}
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
 		}
 	}
 }
