77cb24cd6d4b67f5d1a6517b4dddd0cb329bf9a8
Student6 <Student6@10.8.33.107>
2016-12-12 12:35:58 -0500

    ZOOP

--- Introduction/src/gui/GUIApplication.java
+++ Introduction/src/gui/GUIApplication.java
@@ -4,13 +4,9 @@ import java.awt.Graphics;
 
 import javax.swing.JFrame;
 
-public class GUIApplication extends JFrame {
+public abstract class GUIApplication extends JFrame {
 	
 	private Screen currentScreen;
-	
-	public static void main(String[] args) {
-		new GUIApplication(800, 600);
-	}
 	
 	public GUIApplication(int width, int height) {
 		super();
@@ -21,11 +17,13 @@ public class GUIApplication extends JFrame {
 		setVisible(true);
 	}
 
-	public void initScreen() {
-		currentScreen = new Screen(getWidth(), getHeight());
-	}
+	public abstract void initScreen();
 	
 	public void paint(Graphics g) {
 		g.drawImage(currentScreen.getImage(), 0, 0, null);
+	}
+	
+	public void setScreen(Screen s) {
+		currentScreen = s;
 	}
 }
