7db0b432027c9369929668dca6f32a78b0bdfb17
ghcstking <vlau8050@bths.edu>
2016-12-22 23:06:41 -0500

    ?????

--- Introduction/src/gui/components/ClickableGraphic.java
+++ Introduction/src/gui/components/ClickableGraphic.java
@@ -33,8 +33,8 @@ public class ClickableGraphic extends Graphic implements Clickable, Visible {
 
 	@Override
 	public void act() {
-		if(act != null) {
-			act();
+		if(this.act != null) {
+			this.action.act();
 		}
 	}
 	
