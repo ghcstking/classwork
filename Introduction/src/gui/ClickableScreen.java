b40494ddd9bfef36a9913cae3ea505677438c9ba
Student6 <Student6@10.8.33.107>
2017-01-06 12:13:06 -0500

    cxcvxcv

--- Introduction/src/gui/ClickableScreen.java
+++ Introduction/src/gui/ClickableScreen.java
@@ -1,8 +1,12 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.List;
 
 import gui.components.Clickable;
 import gui.components.Visible;
@@ -10,45 +14,14 @@ import gui.components.Visible;
 public abstract class ClickableScreen extends Screen implements MouseListener {
 
 	private ArrayList<Clickable> clickables;
+	
 	public ClickableScreen(int width, int height) {
 		super(width, height);
+		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		for(int i=0;i<clickables.size();i++){
-			if(clickables.get(i).isHovered(e.getX(),e.getY())){
-				clickables.get(i).act();
-				break;
-			}
-		}
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public abstract void initAllObjects(List<Visible> viewObjects);
+	
 	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
 		initAllObjects(viewObjects);
@@ -59,23 +32,52 @@ public abstract class ClickableScreen extends Screen implements MouseListener {
 			}
 		}
 	}
+
+	public void mouseClicked(MouseEvent m) {
+		for(Clickable c: clickables){
+			if(c.isHovered(m.getX(), m.getY())){
+				c.act();
+				break;
+			}
+		}
+	}
 	
-	public abstract void initAllObjects(ArrayList<Visible> viewObjects);
+	public void addObject(Visible v){
+		super.addObject(v);
+		if(v instanceof Clickable){
+			clickables.add((Clickable)v);
+		}
+	}
 	
+
+	
+	public void remove(Visible v){
+		super.remove(v);
+		clickables.remove(v);
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public MouseListener getMouseListener(){
 		return this;
 	}
 	
-	 public void addObject(Visible v){
-		 super.addObject(v);
-		 if(v instanceof Clickable){
-			 clickables.add((Clickable)v);
-		 }
-	 }
-
-	 
-	 public void remove(Visible v){
-		 super.remove(v);
-		 clickables.remove(v);
-	 } 
 }
--- Introduction/src/whackamole/WhackAMoleScreen.java
+++ Introduction/src/whackamole/WhackAMoleScreen.java
@@ -10,72 +10,75 @@ import gui.components.Visible;
 
 public class WhackAMoleScreen extends ClickableScreen implements Runnable{
 
-	private ArrayList<MoleInterface> moles;
-	private PlayerInterface player;
-	private TextLabel label;
-	private TextLabel timeLabel;
-	private double timeLeft;
+	 private ArrayList<MoleInterface> moles;
+	 private PlayerInterface player;
+	 private TextLabel label;
+	 private TextLabel timeLabel;
+	 private double timeLeft;
 
+	
+	
 	public WhackAMoleScreen(int width, int height) {
 		super(width, height);
 		timeLeft = 30.0;
 		Thread play = new Thread(this);
 		play.start();
 	}
-	
+
 	@Override
-	public void initAllObjects(List<Visible> viewObjects) {
+	public void initAllObjects(ArrayList<Visible> viewObjects) {
 		moles = new ArrayList<MoleInterface>();
 		player = getAPlayer();
-		label = new TextLabel(350,220,100,40,"yeas ");
+		label = new TextLabel(350,220,100,40,"Sample Text");
 		timeLabel = new TextLabel(360,40,80,40,"30.0");
 		viewObjects.add(player);
 		viewObjects.add(timeLabel);
 		viewObjects.add(label);
 	}
 
-	public void update(){
-		super.update();
-	}
-
-	public void changeText(String s){
-		try{
-			label.setText(s);
-			Thread.sleep(1000);
-		}catch(InterruptedException e){
-			e.printStackTrace();
-		}
+	/**
+	 * This method is a placeholder until our "partner"
+	 * finishes the design of Players. For the time being,
+	 * it returns "null". This means our code won't work until
+	 * our partner is done, BUT at least our code will compile
+	 * 
+	 * DON'T FORGET TO CHANGE THIS ONCE YOUR PARTNER IS DONE!
+	 * @return a PlayerInterface for the Screen
+	 */
+	public PlayerInterface getAPlayer(){
+		return new Player();
 	}
 	
-	public PlayerInterface getAPlayer() {
-		return new Player();
+	public MoleInterface getAMole(){
+		return new Mole((int)(Math.random()* getWidth()),
+				(int)(Math.random()*getHeight()));
 	}
 
 	public void run() {
 		changeText("Ready...");
 		changeText("Set...");
 		changeText("Go!");
-		changeText("");
-		timeLabel.setText(""+timeLeft);
+		label.setText("");
+		//since this is a timed game, we will use a while 
+		//loop. This is not necessary for games that 
+		//aren't timed
 		while(timeLeft > 0){
-			try{
-				Thread.sleep(100);
-			}catch(InterruptedException e){
-				e.printStackTrace();
-			}
-			timeLeft -= .1;
-			timeLabel.setText(""+(int)(timeLeft*10)/10.0);
+			updateTimer();
 			updateAllMoles();
 			appearNewMole();
 		}
 	}
-	public void appearNewMole() {
-		double chance = 0.1 * (60-timeLeft)/60;
-		if (Math.random() < chance) {
+
+	private void appearNewMole() {
+		double chance = .1*(60-timeLeft)/60;
+		if(Math.random() < chance){
+			//create a mole
 			final MoleInterface mole = getAMole();
-			mole.setAppearanceTime((int)(500+Math.random()*2000));
-			mole.setAction(new Action() {
-				public void act() {
+			mole.setAppearanceTime(
+					(int)(500+Math.random()*2000));
+			//tell the mole what to do when clicked
+			mole.setAction(new Action(){
+				public void act(){
 					player.increaseScore(1);
 					remove(mole);
 					moles.remove(mole);
@@ -86,27 +89,42 @@ public class WhackAMoleScreen extends ClickableScreen implements Runnable{
 		}
 	}
 
-	/**
-	 * this is a placeholder because early in the game design 
-	 * process, the players aren't designed yet, so
-	 * we use this method later, once we learn how 
-	 * to create a Player
-	 * @return
-	 */
-
-	public void updateAllMoles() {
-		for (int i = 0; i < moles.size(); i++) {
+	private void updateAllMoles() {
+		for(int i = 0; i < moles.size(); i++){
 			MoleInterface m = moles.get(i);
-			int screenTime = m.getAppearanceTime() - 100;
+			int screenTime = m.getAppearanceTime()-100;
 			m.setAppearanceTime(screenTime);
-			if (m.getAppearanceTime() <= 0) {
-				remove(m); //remove from screen
+			if(m.getAppearanceTime() <=0){
+				remove(m);//remove from screen
 				moles.remove(m);
-				i--;
+				i--;//compensate for removal
 			}
+			
 		}
 	}
-	public MoleInterface getAMole() {
-		return new Mole((int)(Math.random() * getWidth()), (int)(Math.random() * getHeight()));
+
+	private void updateTimer() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		timeLeft -= .1;
+		//.1 is not a clean number in binary
+		//so to fix the output, we use this little formula
+		//to round to the nearest 10th.
+		timeLabel.setText(""+(int)(timeLeft*10)/10.0);
+		
 	}
+
+	private void changeText(String string) {
+		label.setText(string);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
