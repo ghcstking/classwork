394a96382777040c2f231f96e171da20ec521e6f
Student6 <Student6@10.8.33.107>
2017-01-10 12:05:42 -0500

    still lol

--- Introduction/src/simon/ButtonInterfaceVicki.java
+++ Introduction/src/simon/ButtonInterfaceVicki.java
@@ -8,6 +8,5 @@ import gui.components.Clickable;
 public interface ButtonInterfaceVicki extends Clickable {
 	public void setAction(Action a);
 	void setColor(int i);
-	void setX(int x);
-	void setY(int y);
+	void setCoords(int x, int y);
 }
