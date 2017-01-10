58bee83f57537b4bbea073f34880097287ae3653
Student6 <Student6@10.8.33.107>
2017-01-10 12:10:35 -0500

    ??

---
+++ Introduction/src/simonVicki/ButtonInterfaceVicki.java
@@ -0,0 +1,12 @@
+package simonVicki;
+
+import java.awt.Color;
+
+import gui.components.Action;
+import gui.components.Clickable;
+
+public interface ButtonInterfaceVicki extends Clickable {
+	public void setAction(Action a);
+	void setColor(int i);
+	void setCoords(int x, int y);
+}
