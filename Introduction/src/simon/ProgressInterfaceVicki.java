b82a29211638e7631650ebd9b36d748121b2dc55
Student6 <Student6@10.8.33.107>
2017-01-10 11:55:15 -0500

    lol idk

--- Introduction/src/simon/ProgressInterfaceVicki.java
+++ Introduction/src/simon/ProgressInterfaceVicki.java
@@ -3,5 +3,9 @@ package simon;
 import gui.components.Visible;
 
 public interface ProgressInterfaceVicki extends Visible {
-
+	public static void gameOver() {
+		
+	}
+	public void setRound(int x);
+	
 }
